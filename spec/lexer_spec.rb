b185b2c623cfc6003c263d417c006a7ce00f87f3
Kaleb McKone <20476319+krmckone@users.noreply.github.com>
2022-10-07 11:00:11 -0400

    Renaming specs

---
+++ spec/lexer_spec.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+RSpec.describe Edir::Lexer do
+  it "has a version number" do
+    expect(Edir::VERSION).not_to be nil
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require "ruby/edi"
+require "edir"
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
