914112a409030dc0ecce2a24d7eff3dfbc1aa6ee
Kaleb McKone <20476319+krmckone@users.noreply.github.com>
2022-10-12 10:03:17 -0400

    Clean up lexer spec

--- spec/lexer_spec.rb
+++ spec/lexer_spec.rb
@@ -1,10 +1,16 @@
 # frozen_string_literal: true
 
-require_relative "./fixtures/lexer_data"
+require_relative "./fixtures/segments_data"
+require_relative "./fixtures/files_data"
+
+tests = {
+  segments: LEXER_SEGMENT_EXAMPLES,
+  files: LEXER_FILE_EXAMPLES
+}
 
 RSpec.describe Edir::Lexer do
   shared_examples "lexing data" do |file, expected_tokens|
-    base_path = "spec/fixtures/segments"
+    base_path = "spec/fixtures/"
     data = File.read("#{base_path}/#{file}")
     it "tokenizes correctly" do
       tokens = Edir::Lexer.new.lex_str(data)
@@ -12,11 +18,15 @@ RSpec.describe Edir::Lexer do
     end
   end
 
-  LEXER_DATA.each do |segment, examples|
-    describe segment do
-      examples.each_with_index do |expected, index|
-        example_file = "#{segment.downcase}/#{segment.downcase}_example_#{index + 1}.txt"
-        it_behaves_like "lexing data", example_file, expected
+  tests.each do |path_prefix, example_types|
+    context path_prefix do
+      example_types.each do |type, examples|
+        describe type do
+          examples.each_with_index do |expected, index|
+            example_file = "#{path_prefix}/#{type.downcase}_example_#{index + 1}.txt"
+            it_behaves_like "lexing data", example_file, expected
+          end
+        end
       end
     end
   end
