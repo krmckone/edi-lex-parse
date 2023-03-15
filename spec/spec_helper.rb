25baec49fca7edf28302f414aa02f0a8efd36faf
Kaleb McKone <20476319+krmckone@users.noreply.github.com>
2023-03-15 13:23:05 -0400

    Clean up spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,39 +14,86 @@ RSpec.configure do |config|
   end
 end
 
+INTERCHANGE_HEADER = [
+  "ISA", "*",
+  "00", "*",
+  "", "*",
+  "00", "*",
+  "", "*",
+  "ZZ", "*",
+  "SHIPPER", "*",
+  "02", "*",
+  "SCAC", "*",
+  "230309", "*",
+  "1640", "*",
+  "U", "*",
+  "00401", "*",
+  "000340000", "*",
+  "O", "*",
+  "P", "*",
+  "`"
+].freeze
+
+INTERCHANGE_FOOTER = [
+  "IEA", "*",
+  "1", "*",
+  "000340000"
+].freeze
+
+FUNC_GROUP_HEADER = [
+  "GS", "*",
+  "SM", "*",
+  "SHIPPER", "*",
+  "SCAC", "*",
+  "20230309", "*",
+  "1620", "*",
+  "80", "*",
+  "X", "*",
+  "004010"
+].freeze
+
+FUNC_GROUP_FOOTER = [
+  "GE", "*",
+  "1", "*",
+  "80"
+].freeze
+
+TRANSAC_SET_HEADER = [
+  "ST", "*",
+  "204", "*",
+  "79902"
+].freeze
+
+TRANSAC_SET_FOOTER = [
+  "SE", "*",
+  "925", "*",
+  "79902"
+].freeze
+
+SEGMENTS = [
+  [
+    "B2", "*",
+    "", "*",
+    "SCAC", "*",
+    "", "*",
+    "124592j3", "*",
+    "", "*",
+    "CC"
+  ],
+  [
+    "B2A", "*",
+    "00", "*",
+    "LT"
+  ]
+].freeze
+
 def build_document
   Edir::Document.new(interchanges: [build_interchange])
 end
 
 def build_interchange
-  header = Edir::Segment.new(
-    [
-      "ISA", "*",
-      "00", "*",
-      "", "*",
-      "00", "*",
-      "", "*",
-      "ZZ", "*",
-      "SHIPPER", "*",
-      "02", "*",
-      "SCAC", "*",
-      "230309", "*",
-      "1640", "*",
-      "U", "*",
-      "00401", "*",
-      "000340000", "*",
-      "O", "*",
-      "P", "*",
-      "`"
-    ]
-  )
-  footer = Edir::Segment.new(
-    [
-      "IEA", "*",
-      "1", "*",
-      "000340000"
-    ]
-  )
+  header = Edir::Segment.new(INTERCHANGE_HEADER)
+  footer = Edir::Segment.new(INTERCHANGE_FOOTER)
   Edir::Interchange.new(
     header: header,
     footer: footer,
@@ -55,26 +102,8 @@ def build_interchange
 end
 
 def build_func_group
-  header = Edir::Segment.new(
-    [
-      "GS", "*",
-      "SM", "*",
-      "SHIPPER", "*",
-      "SCAC", "*",
-      "20230309", "*",
-      "1620", "*",
-      "80", "*",
-      "X", "*",
-      "004010"
-    ]
-  )
-  footer = Edir::Segment.new(
-    [
-      "GE", "*",
-      "1", "*",
-      "80"
-    ]
-  )
+  header = Edir::Segment.new(FUNC_GROUP_HEADER)
+  footer = Edir::Segment.new(FUNC_GROUP_FOOTER)
   Edir::FunctionalGroup.new(
     header: header,
     footer: footer,
@@ -83,35 +112,8 @@ def build_func_group
 end
 
 def build_transac_set
-  header = Edir::Segment.new(
-    [
-      "ST", "*",
-      "204", "*",
-      "79902"
-    ]
-  )
-  footer = Edir::Segment.new(
-    [
-      "SE", "*",
-      "925", "*",
-      "79902"
-    ]
-  )
-  segments = [
-    [
-      "B2", "*",
-      "", "*",
-      "SCAC", "*",
-      "", "*",
-      "124592j3", "*",
-      "", "*",
-      "CC"
-    ],
-    [
-      "B2A", "*",
-      "00", "*",
-      "LT"
-    ]
-  ].map { |e| Edir::Segment.new(e) }
+  header = Edir::Segment.new(TRANSAC_SET_HEADER)
+  footer = Edir::Segment.new(TRANSAC_SET_FOOTER)
+  segments = SEGMENTS.map { |e| Edir::Segment.new(e) }
   Edir::TransactionSet.new(header: header, footer: footer, segments: segments)
 end
